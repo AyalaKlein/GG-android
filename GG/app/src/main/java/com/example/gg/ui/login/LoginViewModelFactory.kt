44ad1d95290a5d106c0dcdecc9a737e20dec0206
AyalaKlein <aya.k.100@gmail.com>
2020-03-26 18:40:27 +0200

    login

---
+++ GG/app/src/main/java/com/example/gg/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.example.gg.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.gg.data.LoginDataSource
+import com.example.gg.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                    loginRepository = LoginRepository(
+                            dataSource = LoginDataSource()
+                    )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
