3adc14766bdb40bf490bf6b25c65bbbe352e0f1e
AyalaKlein <aya.k.100@gmail.com>
2020-03-26 18:57:33 +0200

    all activities

---
+++ GG/app/src/main/java/com/example/gg/EditGame.kt
@@ -0,0 +1,12 @@
+package com.example.gg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditGame : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_game)
+    }
+}
