539a970d1c23d7aaf3f2fa4ede3146cc2455bbf4
assafzvigoren <assafzvigoren4@gmail.com>
2020-03-28 13:52:30 +0300

    add another folder of .idea to gitignore

---
+++ GG/app/src/main/java/com/example/gg/data/model/Comment.kt
@@ -0,0 +1,4 @@
+package com.example.gg.data.model
+
+class Comment {
+}
--- GG/app/src/main/java/com/example/gg/ui/newGame/CreateNewGame.kt
+++ GG/app/src/main/java/com/example/gg/ui/newGame/CreateNewGame.kt
@@ -1,12 +1,12 @@
-package com.example.gg
+package com.example.gg.ui.NewGame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CreateNewGame : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_create_new_game)
     }
 }
