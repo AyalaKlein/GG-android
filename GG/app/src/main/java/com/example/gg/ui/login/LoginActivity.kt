b962351ad94634cacd098e5c12a7235e46abadd0
assafzvigoren <assafzvigoren4@gmail.com>
2020-03-28 14:15:50 +0300

    fix ui display of title in the mainActivity. and arrange imports on androidManifest

--- GG/app/src/main/java/com/example/gg/ui/login/LoginActivity.kt
+++ GG/app/src/main/java/com/example/gg/ui/login/LoginActivity.kt
@@ -15,7 +15,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
-//import com.example.gg.MainActivity
 
 import com.example.gg.R
 import com.example.gg.data.Result
