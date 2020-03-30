36cb9ab89297726a4e8f424a3962756d2434a672
AyalaKlein <aya.k.100@gmail.com>
2020-03-31 00:32:11 +0300

    open game details

--- GG/app/src/main/java/com/example/gg/ui/main/MainActivity.kt
+++ GG/app/src/main/java/com/example/gg/ui/main/MainActivity.kt
@@ -7,14 +7,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.ViewModelProviders
 import com.example.gg.R
 import com.example.gg.data.model.Game
+import com.example.gg.ui.gameDetails.GameDetails
 import com.example.gg.ui.newGame.CreateNewGame
 import kotlinx.android.synthetic.main.activity_game.view.*
-
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
@@ -41,6 +44,7 @@ class MainActivity : AppCompatActivity() {
             games = mainViewModel.getGames()
             afterLoad()
         }).get(MainViewModel::class.java)
+
 
         fab.setOnClickListener { view ->
             val intent = Intent(view.context, CreateNewGame::class.java)
@@ -77,7 +81,10 @@ class MainActivity : AppCompatActivity() {
             var gameView = inflator.inflate(R.layout.activity_game, null)
             gameView.imgFood.setImageResource(R.drawable.ic_launcher_background!!)
             gameView.tvName.text = game.name!!
-
+            gameView.setOnClickListener { v ->
+                context!!.startActivity(Intent(context,GameDetails::class.java))
+                Toast.makeText(context, "You Clicked: " + v.tvName.text,Toast.LENGTH_SHORT).show()
+            }
             return gameView
         }
     }
